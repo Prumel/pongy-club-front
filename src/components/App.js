f6551d31ae4b39df08a1be86c04c35ff6d8944d0
Ludi <melissaljf@orange.fr>
2024-04-25 16:50:50 +0200

    config du front avec react part 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
 import React from "react";
-class Toto extends React.Component {
-  render() {
-    return (
-      <div className="toto">
-        Hello Toto !
-      </div>
-    );
-  }
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+export default function App() {
+
+  return (
+   <header className='d-flex justify-content-center align-items-center'>
+     <h1>Pongy Club</h1>
+   </header>
+
+   <BrowserRouter>
+        <Navbar
+            className='mb-5'
+            collapseOnSelect="true"
+            bg='black'
+            variant='dark'
+            sticky='top'
+            expand='md'
+        >
+            <Container>
+                <Navbar.Brand href='/'>Pongy Club</Navbar.Brand>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                <Navbar.Collapse id='responsive-navbar-nav'>
+
+                    <Nav>
+                        <Nav.Link as={Link} eventKey='1' to="/welcome">
+                            <i className='fa fa-home me-2'></i>
+                            Accueil
+                        </Nav.Link>
+                        <Nav.Link as={Link} eventKey='2' to="/connection">
+                            <i className='fa fa-home me-2'></i>
+                            Accueil
+                        </Nav.Link>
+
+
+   </BrowserRouter>
+  );
 }
-const App = () => (<Toto />);
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+
+import "./css/fontawesome.all.min.css";
+import "./css/cosmo.min.css";
+import "./css/style.css";
+
+export const myContext = createContext();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
