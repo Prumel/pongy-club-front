11e560b65dc1bd3e48a285391a8f2b3b9e8dc5a9
Ludi <melissaljf@orange.fr>
2024-04-25 14:21:40 +0200

    config du front avec react

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Toto extends React.Component {
+  render() {
+    return (
+      <div className="toto">
+        Hello Toto !
+      </div>
+    );
+  }
+}
+const App = () => (<Toto />);
+export default App;
