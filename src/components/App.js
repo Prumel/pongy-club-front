601677dde24f69115caf6fc82b3a563891861eb0
Lulu <melissaljf@orange.fr>
2024-05-31 00:09:50 +0200

    upgrade footer and welcome page

--- src/components/App.js
+++ src/components/App.js
@@ -16,79 +16,87 @@ export default function App() {
 
   return (
 
-<myContext.Provider value={[member, setMember]}>
-   <header className='my-3 d-flex justify-content-center align-items-center'>
-     <h1>Pongy Club<i className='ms-3 me-3 fas fa-table-tennis'></i></h1>
-   </header>
+    <myContext.Provider value={[member, setMember]}>
+       <header className='my-3 d-flex justify-content-center align-items-center'>
+         <h1>Pongy Club<i className='ms-3 me-3 fas fa-table-tennis'></i></h1>
+       </header>
 
-   <BrowserRouter>
-        <Navbar
-            className='mb-5'
-            collapseOnSelect="true"
-            style={{backgroundColor: '#D8572A'}}
-            variant='dark'
-            sticky='top'
-            expand='md'
-        >
-            <Container>
-                <Navbar.Brand href='/'>Pongy Club</Navbar.Brand>
-                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-                <Navbar.Collapse id='responsive-navbar-nav'>
+       <BrowserRouter>
+            <Navbar
+                className='mb-5'
+                collapseOnSelect="true"
+                style={{backgroundColor: '#D8572A'}}
+                variant='dark'
+                sticky='top'
+                expand='md'
+            >
+                <Container>
+                    <Navbar.Brand href='/'>Pongy Club</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Collapse id='responsive-navbar-nav'>
 
-                    <Nav>
-                        <Nav.Link as={Link} eventKey='1' to="/welcome">
-                            <i className='fa fa-home me-1'></i>Accueil
-                        </Nav.Link>
-                        <Nav.Link as={Link} eventKey='2' to="/connection" hidden={member !== null}>
-                            <i className='fa fa-lock me-1'></i>
-                            Se connecter
-                        </Nav.Link>
-                        <Nav.Link as={Link} eventKey='3' to="/register" hidden={member !== null}>
-                            <i className='fas fa-user-plus me-1'></i>
-                            Créer un compte
-                        </Nav.Link>
-                        <Nav.Link as={Link} eventKey='4' to="/welcome" hidden={member === null} onClick={() => {setMember(null);}}>
-                            <i className='fa fa-unlock me-1'></i>
-                            Déconnexion
-                        </Nav.Link>
-                        <Nav.Link as={Link} eventKey='5' to="/admin">
-                            Admin
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                        <Nav>
+                            <Nav.Link as={Link} eventKey='1' to="/welcome">
+                                <i className='fa fa-home me-1'></i>Accueil
+                            </Nav.Link>
+                            <Nav.Link as={Link} eventKey='2' to="/connection" hidden={member !== null}>
+                                <i className='fa fa-lock me-1'></i>
+                                Se connecter
+                            </Nav.Link>
+                            <Nav.Link as={Link} eventKey='3' to="/register" hidden={member !== null}>
+                                <i className='fas fa-user-plus me-1'></i>
+                                Créer un compte
+                            </Nav.Link>
+                            <Nav.Link as={Link} eventKey='4' to="/welcome" hidden={member === null} onClick={() => {setMember(null);}}>
+                                <i className='fa fa-unlock me-1'></i>
+                                Déconnexion
+                            </Nav.Link>
+                            <Nav.Link as={Link} eventKey='5' to="/admin">
+                                Admin
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <article>
+                <Container>
+                    <Routes>
+                        <Route path='/' element={<Welcome />} />
+                        <Route path='/welcome' element={<Welcome />} />
+                        <Route path='/connection' element={<ConnectionController />} />
+                        <Route path='/register' element={<RegistrationController />} />
+                        <Route path='/admin' element={<AdminController />} />
+                    </Routes>
+                </Container>
+            </article>
+       </BrowserRouter>
 
-        <article>
-            <Container>
-                <Routes>
-                    <Route path='/' element={<Welcome />} />
-                    <Route path='/welcome' element={<Welcome />} />
-                    <Route path='/connection' element={<ConnectionController />} />
-                    <Route path='/register' element={<RegistrationController />} />
-                    <Route path='/admin' element={<AdminController />} />
-                </Routes>
-            </Container>
-        </article>
-   </BrowserRouter>
 
-   <div className="footer" style={{backgroundColor: '#D8572A'}}>
-        <h3>Contactez-nous !</h3>
-            <p> <a href="mailto:exemple@gmail.com"><i className="me-1 fa-solid fa-envelope"></i></a> Email : <a href="mailto:exemple@gmail.com">pongyclub@gmail.com</a></p>
-            <p><i className="me-1 fa-solid fa-phone"></i> Téléphone : 01 23 45 67 89</p>
-            <div className="map-container">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9784984478433!2d2.7708389760003387!3d48.85862040069097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e61cd870b432b3:0x1d754b3187d4d77a!2s9%20Rue%20du%20Fossé%20Mignard,77700%20Chessy!5e0!3m2!1sfr!2sfr!4v1714134228047!5m2!1sfr!2sfr"
-                    width="600"
-                    height="450"
-                    style={{ border: "0" }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade">
-                </iframe>
+      <footer style={{backgroundColor: '#D8572A'}}>
+        <Container>
+            <div className="row">
+            <div className="col-md-6 d-flex-column">
+                <h4 style={{paddingTop: '20px'}} >Contactez-nous !</h4>
+                <p> <a href="mailto:exemple@gmail.com" style={{color:"inherit"}}><i className="me-1 fa-solid fa-envelope" ></i></a> Email : <a href="mailto:exemple@gmail.com" style={{color:"inherit"}}>pongyclub@gmail.com</a></p>
+                <p><i className="me-1 fa-solid fa-phone"></i> Téléphone : 01 23 45 67 89</p>
+                <p><i className="me-1 fa-solid fa-map-marker"></i> Adresse : 9 rue du Fossé Mignard, 77700 Chessy</p>
+                <p><a href="https://www.facebook.com/FFTTofficiel" style={{textDecoration:"none"}} style={{color:"inherit"}}> <i className="me-1 fa-solid fa-brands fa-facebook-f"></i>Suivez-nous sur Facebook</a> </p>
             </div>
-   </div>
+                 <div className="col-md-6 d-flex justify-content-center align-items-center p-3">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9784984478433!2d2.7708389760003387!3d48.85862040069097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e61cd870b432b3:0x1d754b3187d4d77a!2s9%20Rue%20du%20Fossé%20Mignard,77700%20Chessy!5e0!3m2!1sfr!2sfr!4v1714134228047!5m2!1sfr!2sfr"
+                        width="auto"
+                        height="auto"
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
+            </div>
+        </Container>
 
-</myContext.Provider>
+      </footer>
+
+    </myContext.Provider>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,6 @@
 import React from "react";
+import raquetteBalleTable from 'public/raquette_balle_table.png';
+import banniereFB from 'public/couv.png';
 
 export default function Welcome(props) {
 
@@ -7,7 +9,28 @@ export default function Welcome(props) {
             <h2 className=" my-3 d-flex justify-content-center align-items-center">Bienvenue au Pongy Club</h2>
             <h3>Présentation du club</h3>
 
-            <p>lorem ipsum</p>
+            <div className="row">
+                <div className="col-md d-flex justify-content-center align-items-center p-3">
+                    <img src={raquetteBalleTable} alt="Raquette, balle et table de ping-pong" />
+                </div>
+                <div className="col-md d-flex justify-content-center align-items-center p-3">
+                    <p>Pongy Club est un club de tennis de table rattaché à la <a href="https://www.fftt.com/"> FFTT</a>
+                    - Fédération Française de Tennis de Table et à la <a href="https://www.fnsmr.org/">FNSMR</a> -
+                    Fédération Nationale Sport en Milieu Rural.
+                    <br/>
+                    Le club est ouvert à tous les publics, de 7 à 100 ans et plus,
+                    et propose des entraînements libres pour les adultes et des entraînements supervisés pour
+                    les enfants.</p>
+
+
+                </div>
+            </div>
+            <h3>Découvrez nos partenaires</h3>
+            <p>Le Pongy Club est soutenu par plusieurs partenaires qui nous permettent d'organiser des événements </p>
+            - BTP -
+            - Immobilier -
+            <h3>Restez à la page !</h3>
+                <img src={banniereFB} alt="Bannière suivez-nous sur facebook" />
 
         </>
     );
